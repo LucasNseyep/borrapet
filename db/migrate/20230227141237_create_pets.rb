977c9c0a7ebfd3b9c7d39cc96a3b1db77b6ba6ac
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-27 16:44:30 +0000

    git commands are hard

--- db/migrate/20230227141237_create_pets.rb
+++ db/migrate/20230227141237_create_pets.rb
@@ -3,6 +3,9 @@ class CreatePets < ActiveRecord::Migration[7.0]
     create_table :pets do |t|
       t.references :user, null: false, foreign_key: true
       t.string :pet_type
+      t.string :name
+      t.string :breed
+      t.string :bio
 
       t.timestamps
     end
