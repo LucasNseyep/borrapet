bace29a63579c5754a7bb5e0174925bef8554bf0
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-27 12:29:07 +0000

    set up done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # TO BE CHANGED ONCE THE HEROKU IS SET UP
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
