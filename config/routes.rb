975791ba23647c9edd37a62cfc478235d2d20cec
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-27 14:31:11 +0000

    pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pets/index'
+  get 'pets/new'
+  get 'pets/create'
+  get 'pets/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
