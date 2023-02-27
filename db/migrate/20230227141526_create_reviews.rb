e28bd681221eeb3add37ea6ff16c2d6fc9d32a7f
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-02-27 14:17:33 +0000

    issue with review paydate reference, correction play_date

--- db/migrate/20230227141526_create_reviews.rb
+++ db/migrate/20230227141526_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-      t.references :playdate, null: false, foreign_key: true
+      t.references :play_date, null: false, foreign_key: true
       t.integer :rating
       t.text :content
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_122516) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_141526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "pet_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
+  create_table "play_dates", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_play_dates_on_pet_id"
+    t.index ["user_id"], name: "index_play_dates_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "play_date_id", null: false
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["play_date_id"], name: "index_reviews_on_play_date_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,11 +52,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_122516) do
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
     t.string "address", default: "", null: false
-    t.string "description"
+    t.string "description", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users"
+  add_foreign_key "play_dates", "pets"
+  add_foreign_key "play_dates", "users"
+  add_foreign_key "reviews", "play_dates"
 end
