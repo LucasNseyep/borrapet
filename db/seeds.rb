8a47e2c32b2f3b065a5aebec65b6b4e9431290b3
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-02-27 15:41:37 +0000

    Added user seeds and faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users"
+
+100.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  user = User.create(
+    email: "#{first_name.downcase}.#{last_name.downcase}@borrapet.com",
+    password: "1234567",
+    phone_number: Faker::PhoneNumber.cell_phone,
+    first_name: first_name,
+    last_name: last_name,
+    address:Faker::Address.full_address
+  )
+
+  puts "#{first_name} #{last_name} was added!"
+end
+
+puts "All done with the users"
