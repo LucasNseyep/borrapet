2cc0c2dc5a66e201ee6e941e6aba2802f430fafa
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-02-28 17:09:40 +0000

    Fix pet bio on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,7 @@ o'')}____//
 
  \n\n\n"
 
+ description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
 10.times do
   pet_name = Faker::Creature::Dog.name
