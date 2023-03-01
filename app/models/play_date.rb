34deebfb8d29f2cfc2bdb31f0d3edb48e8526939
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-03-01 13:14:26 +0000

    added uniqueness to play_dates and tested for it

--- app/models/play_date.rb
+++ app/models/play_date.rb
@@ -3,4 +3,7 @@ class PlayDate < ApplicationRecord
   belongs_to :pet
 
   has_many :reviews
+
+  validates :start_time, :end_time, presence: true
+  validates :start_time, :end_time, uniqueness: true
 end
